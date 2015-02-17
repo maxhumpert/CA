fd57d258fb27d224b456a933187dd401147285d0
Max Humpert <max.humpert@fh-muenster.de>
2015-02-17 19:46:35 +0100

    fixing

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,15 @@ Category.create name: 'Indoor'
 Category.create name: 'Outdoor'
 Category.create name: 'Party'
 
+pass = pass1234
+admin = User.create email: 'max.humpert@fh-muenster.de', password: pass,
+                    password_confirmation: pass
+admin.add_role :admin
+puts "Admin password is #{pass}"
+
+pass = pass12345
+admin = User.create email: 'sergeifladung@googlemail.com', password: pass,
+                    password_confirmation: pass
+admin.add_role :admin
+puts "Admin password is #{pass}"
+
