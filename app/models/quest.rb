3d7d1ca99260a4298436374363208995eca08841
Napfkuchen <sergeifladung@googlemail.com>
2015-02-20 00:59:54 +0100

    Spec fuer Quest repariert und Spec fuer User erstellt

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -2,5 +2,5 @@ class Quest < ActiveRecord::Base
   has_many :quest_categories
   has_many :categories, through: :quest_categories
   validates :title, :description, :points, presence: true, uniqueness: true
-  validates :points, numericality: {:greater_than_or_equal => 10, :less_than_or_equal => 300, :only_integer => true}
+  validates :points, numericality: {:greater_than_or_equal_to => 10, :less_than_or_equal_to => 300, :only_integer => true}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,61 @@
+# == Schema Information
+#
+# Table name: users
+#
+# id :integer not null, primary key
+# name :string(255)
+# email :string(255)
+# created_at :datetime
+# updated_at :datetime
+#
+
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = {
+        # :name => "Example User",
+        :email => "charles@schulzkowski.com",
+        :password => "12345678",
+        :password_confirmation => "12345678"
+    }
+  end
+  it "should create a new instance given valid attributes" do
+    User.create!(@attr)
+  end
+
+  it "should require an email" do
+    no_email_user = User.new(@attr.merge(:email => ''))
+    expect(no_email_user).to_not be_valid
+  end
+
+  it "should accept valid email addresses" do
+    invalid_email_user = User.new(@attr.merge(:email => 'charles@schulzkowski.'))
+    expect(invalid_email_user).to_not be_valid
+  end
+
+  it "should reject duplicate email addresses" do
+    User.create!(@attr)
+    user_with_duplicate_email = User.new(@attr)
+    expect(user_with_duplicate_email).to_not be_valid
+  end
+
+  describe "password validations" do
+    it "should require a password" do
+      user_without_password = User.new(@attr.merge(:password => "", :password_confirmation => ""))
+      expect(user_without_password).to_not be_valid
+    end
+
+    it "should require a matching password confirmation" do
+      user_with_failing_confirmation = User.new(@attr.merge(:password_confirmation => "invalid"))
+      expect(user_with_failing_confirmation).to_not be_valid
+
+    end
+    it "should not accept short password" do
+      short = "a" * 7
+      user_with_short_pass = User.new(@attr.merge(:password => short, :password_confirmation => short))
+      expect(user_with_short_pass).to_not be_valid
+    end
+  end
+
 end
