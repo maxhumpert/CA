94cad4c3f7cc0167b920803804010561c54c4313
Napfkuchen <sergeifladung@googlemail.com>
2015-02-27 07:30:44 +0100

    Some Layout-fixing

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,10 +26,40 @@ Quest.create(:title => 'Zimmer aufräumen',
 # . . .
 
 Quest.create(:title => 'Nackte Meile',
-               :description => %{Laufe um Mitternacht einmal um den Campus, nackt!},
+               :description => %{ Laufe um Mitternacht einmal um den Campus, nackt! },
                :points => 150,
                :user_id => 9,
                :category => Category.find(1))
+
+Quest.create(:title => ' Old Selfie ',
+               :description => %{ Make a selfie infront of the oldest building in Münster. },
+               :points => 150,
+               :category => Category.find(1))
+
+Quest.create(:title => ' Animal Beer ',
+               :description => %{ Drink a beer in every bar with an animal in their name (e.g. Peacock). },
+               :points => 250,
+               :category => Category.find(1))
+
+Quest.create(:title => ' Promenade-Jogging ',
+               :description => %{ Beat my time in jogging one round around the Promenade Muenster (15min20sec). },
+               :points => 100,
+               :category => Category.find(1))
+
+Quest.create(:title => ' Nuts? ',
+               :description => %{ Come 10 minutes late to your next lecture wearing a carnival costume of your choice. },
+               :points => 300,
+               :category => Category.find(0))
+
+Quest.create(:title => ' Dry Mouth',
+               :description => %{ Eat a slice of dry toast in less than 30 seconds. },
+               :points => 50,
+               :category => Category.find(0))
+
+Quest.create(:title => ' In love with your professor ',
+               :description => %{ Ask your professor: „In what bar did you went last night? You look like it was a good night!. },
+               :points => 150,
+               :category => Category.find(0))
 
 User.delete_all
 pass = "pass1234"
